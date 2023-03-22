71a19a5c0044dfc492cd61cdf6a2d3b6a62019a0
Laura Bährs <laura.baehrs@googlemail.com>
2023-03-22 23:40:18 +0100

    add hero for homepage

--- front-page.php
+++ front-page.php
@@ -6,9 +6,14 @@ while (have_posts()) {
     the_post();
 ?>
 
-    <div class="container">
-        <h1>Home</h1>
-    </div>
+
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Welkom bij de
+                Social Brothers case!</h1>
+            <p>Deze opdracht dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. </p>
+        </div>
+    </section>
 
 <?php
 }
