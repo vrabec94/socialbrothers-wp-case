0fc3723c8c09e9c5a2217142962e74085e69004b
Laura Bährs <laura.baehrs@googlemail.com>
2023-03-22 22:29:02 +0100

    add dependencies and header

--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,20 @@
 <?php defined('ABSPATH') || exit('Forbidden'); ?>
 
 <header>
-  <div class="container">
-    Header
+  <div class="header-container">
+    <div class="header-logo">
+    <a href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logo.png'); ?>" alt="Social Brothers">
+      </a>
+    </div>
+    <div class="menu">
+      <a href="/blog">Blog</a>
+      <a href="/events">Events</a>
+      <div class="search-icon">
+					<img src="search.png" alt="Search Icon">
+				</div>
+    </div>
+    <button type="button" id="login-btn">Login</button>
   </div>
 </header>
 
