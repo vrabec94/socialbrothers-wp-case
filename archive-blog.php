7a331ac89dcf83f5daceed4b6fdfb4441fa68b18
Laura Bährs <laura.baehrs@googlemail.com>
2023-03-23 02:49:49 +0100

    add bootstrap cards layout

--- archive-blog.php
+++ archive-blog.php
@@ -7,9 +7,19 @@ get_header();
     <h1>Blog archief</h1>
 </div>
 <?php
+$counter = 1;
 if (have_posts()) :
     while (have_posts()) : the_post();
+        if ($counter % 3 == 1) {
+            // Start a new row
+            echo '<div class="row">';
+        }
         get_template_part('partials/card');
+        if ($counter % 3 == 0 || $counter == $wp_query->post_count) {
+            // Close the row
+            echo '</div>';
+        }
+        $counter++;
     endwhile;
 endif;
 ?>
--- partials/card.php
+++ partials/card.php
@@ -4,16 +4,22 @@
   <?php the_content(); ?>
 </div>
 -->
-
-<div class="card" style="width: 18rem;">
-    <?php if (has_post_thumbnail()) : ?>
-        <a href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => 'Featured image']); ?>
-        </a>
-    <?php endif; ?>
-    <div class="card-body">
-        <h5 class="card-title"><?php the_title(); ?></h5>
-        <p class="card-text"><?php the_content(); ?></p>
-        <a href="<?php the_permalink(); ?>">Lees meer</a>
+<div class="col-4">
+    <div class="card" style="width: 18rem;">
+        <?php if (has_post_thumbnail()) : ?>
+            <div class="card-img-top" style="position: relative;">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium', ['alt' => 'Featured image']); ?>
+                    <span class="badge badge-primary" style="position: absolute; top: 10px; left: 10px;">
+                        <?php echo get_the_category_list(', '); ?>
+                    </span>
+                </a>
+            </div>
+        <?php endif; ?>
+        <div class="card-body">
+            <h5 class="card-title"><?php the_title(); ?></h5>
+            <p class="card-text"><?php the_content(); ?></p>
+            <a href="<?php the_permalink(); ?>">Lees meer</a>
+        </div>
     </div>
 </div>
